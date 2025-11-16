07e87fd27dedce47dd90c45d2b074dc9d6d0c4f0
Lindsey Bedwell <lindseybedwell@Mac.lan>
2025-11-15 22:40:16 -0800

    initial commit to test connection

---
+++ r/basic_etl_process.R
@@ -0,0 +1,9 @@
+### TITLE:    basic ETL process
+### PURPOSE:  demonstrate an ETL process that uses an external source to extract the data,
+###           transforms data into format using business logic, and loads into a target database object.
+###           This process can also be set up to utilize what has already been built by Python 
+###           and connect to the database/data.
+
+# load libraries and connections ------------------------------------------
+
+print(paste0("This is a test to confirm Git connection is working."))
