66af3635e915ebb529d7b0fadd81d55a03c7503b
Lindsey Bedwell <lindseybedwell@Mac.lan>
2025-11-16 22:04:54 -0800

    add common functions and continue setup of firts etl process

--- r/basic_etl_process.R
+++ r/basic_etl_process.R
@@ -6,20 +6,104 @@
 
 # load libraries and connections ------------------------------------------
 
-library(data.table)
-library(DBI)
-library(RSQLite)
-library(dplyr)
-library(dbplyr)
-library(readr)
-library(openxlsx)
+### bring in common set of functions and libraries for setup
+source('r/common_functions.R')
 
 ### connect to the database and target table
 db_con <- dbConnect(RSQLite::SQLite(), paste0("data/local_warehouse.db"))
 
-### read in the existing table (if it's already loaded from the python process)
-registry_costs_db <- data.frame(dbGetQuery(conn = db_con, statement = paste0("select * from registry_costs_fact")))
+### default current reporting period
+fiscal_period_repl <- c(
+  start_fperiod = conv_date_to_fiscal_period(rollback(Sys.Date()))
+  , end_fperiod = conv_date_to_fiscal_period(rollback(Sys.Date()))
+)
 
 ### read in the external data
-registry_costs_df <- read.csv(file = 'data/registry_costs.csv')
+# rm(registry_costs_df)
+registry_costs_df <- data.table(read_csv(file = 'data/registry_costs.csv')) %>% 
+  .[,.(
+    entity = as.character(entity)
+    , department = as.character(department)
+    , employee_name = as.character(employee_name)
+    , invoice_date = mdy(invoice_date)
+    , hours = as.numeric(hours)
+    , dollars = as.numeric(dollars)
+    
+    )] %>% .[]
+
+registry_costs_df[,`:=`(fiscal_period = conv_date_to_fiscal_period(invoice_date)
+                  , tbl_src = 'external'
+                  )] %>% .[]
+
+### read in the existing table (if it's already loaded from the python process)
+rm(registry_costs_db)
+registry_costs_db <- data.table(dbGetQuery(conn = db_con
+                                           , statement = paste0("select * from registry_costs_fact"))) %>%
+  .[,.(
+    entity = as.character(entity)
+    , department = as.character(department)
+    , employee_name = as.character(employee_name)
+    , invoice_date = as.Date(invoice_date)
+    , hours = as.numeric(hours)
+    , dollars = as.numeric(dollars)
+    , fiscal_period = as.character(fiscal_period)
+    )] %>% 
+  .[, tbl_src := 'registry_costs_fact']
+
+
+# data comparison ----------------------------------------------------------
+
+### run validation on what is already in the table and what needs to be updated/added
+head(registry_costs_df, 10)
+head(registry_costs_db, 10)
+
+registry_diffs <- 
+  rbind(registry_costs_df
+        , registry_costs_db) %>%
+  dcast(.,...~tbl_src, fill = 0, value.var = c('hours', 'dollars')) %>% 
+  .[,`:=`(hours_diff = hours_registry_costs_fact - hours_external
+          , dollars_diff = dollars_registry_costs_fact - dollars_external
+          )] %>% 
+  .[,diff_flag := fifelse(hours_diff != 0 & dollars_diff != 0, 1, 0)] %>% 
+  .[diff_flag == 1]
+
+start_fperiod <- min(unique(registry_diffs$fiscal_period))
+fiscal_period_repl[1] <- start_fperiod
+
+
+
+# load to database --------------------------------------------------------
+
+### after comparing what would need to be updated, remove data from periods that are in the parameter
+dbGetQuery(conn = db_con
+           , statement = paste0("DELETE FROM registry_costs_fact
+                                where fiscal_period between '", fiscal_period_repl[1]
+                                ,"' and '", fiscal_period_repl[2],"' ")
+           )
+dbExecute(conn = db_con
+          , statement = )
+### load data to the target table for the periods that need to be loaded
+dbWriteTable(conn = db_con
+             , name = Id( table = "registry_costs_fact")
+             , value = as.data.frame(registry_costs_df[between(fiscal_period
+                                                               , fiscal_period_repl[1]
+                                                               , fiscal_period_repl[2])
+                                                       ,.(entity
+                                                          , department
+                                                          , employee_name
+                                                          , invoice_date
+                                                          , hours
+                                                          , dollars
+                                                          , fiscal_period
+                                                          )]) 
+             , append = TRUE # note that this is to append the table 
+             )
+
+
+# post-load validation ----------------------------------------------------
+
+data.table(dbGetQuery(conn = db_con
+                      , statement = paste0("select * from registry_costs_fact")))
+
+
 
--- r/common_functions.R
+++ r/common_functions.R
@@ -1,6 +1,49 @@
-### common functions
+### TITLE:    common functions and libraries
+### PURPOSE:  utilize this common set of functions and libraries for setup
 
-# conv_fiscal_period_to_date
-# 
-# 
-# conv_date_to_fiscal_period
+# common libraries --------------------------------------------------------
+
+library(data.table)
+library(DBI)
+library(RSQLite)
+library(dplyr)
+library(dbplyr)
+library(readr)
+library(openxlsx)
+library(lubridate)
+
+# common functions --------------------------------------------------------
+
+### convert the fiscal period to a date format (using October as the fiscal year start)
+conv_fiscal_period_to_date <- function(fiscal_period) {
+  fiscal_year <- as.integer(substr(fiscal_period, 1, 4))
+  fiscal_month <- as.integer(substr(fiscal_period, 5, 6))
+  cal_year <- fifelse(between(fiscal_month, 1, 3)
+                      , fiscal_year - 1
+                      , fiscal_year
+                      )
+  cal_month <- fifelse(between(fiscal_month, 1, 3)
+                       , fiscal_month + 9
+                       , fiscal_month + 9 - 12
+                       )
+  
+  print(as.Date(sprintf("%04d-%02d-01", cal_year, cal_month)))
+
+}
+
+### convert the date to a fiscal period format (using October as the fiscal year start)
+conv_date_to_fiscal_period <- function(date) {
+  date <- as.Date(date)
+  cal_month <- as.integer(format(date, "%m"))
+  cal_year <- as.integer(format(date, "%Y"))
+  fiscal_month <- fifelse(between(cal_month, 10, 12)
+                          , cal_month - 9
+                          , cal_month + 3
+                          )
+  fiscal_year <- fifelse(cal_month < 10
+                         , cal_year 
+                         , cal_year + 1)
+  
+  print(as.character(sprintf("%d%02d", fiscal_year, fiscal_month)))
+
+}
